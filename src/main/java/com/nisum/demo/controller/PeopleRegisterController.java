4eabe5c30c4492c8aae3f809b7670ac0204ea05d
Francisca <franciscaljubetic@gmail.com>
2022-04-25 21:52:31 -0400

    Merge remote-tracking branch 'origin/fljubetic-v2' into fljubetic-v2

--- src/main/java/com/nisum/demo/controller/PeopleRegisterController.java
+++ src/main/java/com/nisum/demo/controller/PeopleRegisterController.java
@@ -1,7 +1,6 @@
 package com.nisum.demo.controller;
 
 import com.nisum.demo.model.People;
-import com.nisum.demo.repository.PeopleRepository;
 import com.nisum.demo.service.PeopleRegisterService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +11,16 @@ import java.util.ArrayList;
 //Rest Controller
 @RestController
 public class PeopleRegisterController {
+
     @GetMapping("/")
-    public static People getPeople(){
-
-        PeopleRepository.setRepository();
-
-        PeopleRegisterService.setFamilyTree();
+    public static ArrayList<People> getPeople(){
 
         return PeopleRegisterService.getPeople();
     }
 
-    @GetMapping("/query<18")
+    @GetMapping("/query>=18")
     public static ArrayList<People> queryPeopleByAge(){
 
-        return PeopleRegisterService.queryPeopleByAge(18, PeopleRepository.getRepository());
+        return PeopleRegisterService.queryPeopleByAge();
     }
 }
--- src/test/java/com/nisum/demo/service/PeopleRegisterServiceTest.java
+++ src/test/java/com/nisum/demo/service/PeopleRegisterServiceTest.java
@@ -3,17 +3,19 @@ package com.nisum.demo.service;
 import com.nisum.demo.model.People;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PeopleRegisterServiceTest {
     @Test
     public void getPeopleTest() {
-        People peopleExpectedResult = new People();
-        peopleExpectedResult.setName("juan");
-        peopleExpectedResult.setLastname("soto");
-        peopleExpectedResult.setRut("1365445-9");
+        ArrayList<People> peopleExpectedResult = new ArrayList<>();
+        /*peopleExpectedResult.get(0);
+        /*peopleExpectedResult.setLastname("soto");
+        peopleExpectedResult.setRut("1365445-9");*/
 
-        People actualResult = PeopleRegisterService.getPeople();
+        ArrayList<People> actualResult = PeopleRegisterService.getPeople();
 
         assertEquals(peopleExpectedResult, actualResult);
     }
