d2ff6d050e5926885e57e453c8735e1dd4e97eee
Francisca <franciscaljubetic@gmail.com>
2022-04-25 21:52:31 -0400

    Merge remote-tracking branch 'origin/fljubetic-v2' into fljubetic-v2

--- src/main/java/com/nisum/demo/repository/PeopleRepository.java
+++ src/main/java/com/nisum/demo/repository/PeopleRepository.java
@@ -1,37 +1,59 @@
 package com.nisum.demo.repository;
 
 import com.nisum.demo.model.People;
-import com.nisum.demo.service.PeopleRegisterService;
 
 import java.util.ArrayList;
 
 public class PeopleRepository {
 
-    private static final ArrayList<People> allPeople = new ArrayList<>();
+    public static ArrayList<People> allPeople = new ArrayList<>();
 
     public static void setRepository() {
 
         People Rosa = new People("Rosa", "Martinez", "789652365425-2", 58,null);
-
         People Gladys = new People("GLadys", "Del Río", "52365425-2", 21,null);
-
         People Fran = new People("Francisca", "Lju", "565425-2", 76,null);
-
         People Juana = new People("Juana", "lopez", "165748202-3", 99,null);
-
         People Fede = new People("Federico", "martinez", "7365425-2", 17,null);
-
         People Anggelo = new People("Anggelo", "Urso", "27365425-2", 11,null);
-
         People Alicia = new People("Alicia", "Zamorano", "27378425-2", 15,null);
+        People Adalberto = new People( "Adalberto", "Faundez",  "4415498", 105, null);
+        People Silvestrito = new People( "Silvestrito", "Faundez",  "294415498", 3, null);
 
-        allPeople.add(Rosa);
-        allPeople.add(Gladys);
-        allPeople.add(Fran);
-        allPeople.add(Juana);
-        allPeople.add(Fede);
-        allPeople.add(Anggelo);
-        allPeople.add(Alicia);
+        PeopleRepository.allPeople.add(Rosa);
+        PeopleRepository.allPeople.add(Gladys);
+        PeopleRepository.allPeople.add(Fran);
+        PeopleRepository.allPeople.add(Juana);
+        PeopleRepository.allPeople.add(Fede);
+        PeopleRepository.allPeople.add(Anggelo);
+        PeopleRepository.allPeople.add(Alicia);
+        PeopleRepository.allPeople.add(Adalberto);
+        PeopleRepository.allPeople.add(Silvestrito);
+    }
+
+    public static void setSons(People dad, People son, People son2) {
+
+        ArrayList<People> sonsList = new ArrayList<>();
+        sonsList.add(son);
+        sonsList.add(son2);
+        dad.setSon(sonsList);
+    }
+
+    public static void setFamilyTree() {
+        People people = PeopleRepository.allPeople.get(0);
+
+        People h1 = PeopleRepository.allPeople.get(1);
+        People h2 = PeopleRepository.allPeople.get(2);
+
+        People h1h1 = PeopleRepository.allPeople.get(3);
+        People h1h2 = PeopleRepository.allPeople.get(4);
+
+        People h2h1 = PeopleRepository.allPeople.get(5);
+        People h2h2 = PeopleRepository.allPeople.get(6);
+
+        setSons(people, h1, h2);
+        setSons(h1, h1h1, h1h2);
+        setSons(h2, h2h1, h2h2);
     }
 
     public static ArrayList<People> getRepository() {
--- src/main/java/com/nisum/demo/service/PeopleRegisterService.java
+++ src/main/java/com/nisum/demo/service/PeopleRegisterService.java
@@ -6,61 +6,34 @@ import com.nisum.demo.repository.PeopleRepository;
 import java.util.ArrayList;
 
 
-public class PeopleRegisterService {
+public class PeopleRegisterService{
 
-    public static People getPeople() {
-
-        ArrayList<People> allPeopleTogether = PeopleRepository.getRepository();
-
-        return allPeopleTogether.get(0);
+    public static ArrayList<People> getPeople() {
+        PeopleRepository.setRepository();
+        PeopleRepository.setFamilyTree();
+        return PeopleRepository.getRepository();
     }
 
-    public static People getPeople(int index) {
+    /*public static People getPeopleBythe() {
+        PeopleRepository.setRepository();
+        PeopleRepository.setFamilyTree();
+        ArrayList<People> allPeople = PeopleRepository.getRepository();
+        return allPeople.get(0);
+    }*/
 
-        ArrayList<People> allPeopleTogether = PeopleRepository.getRepository();
-
-        return allPeopleTogether.get(index);
-
-    }
-
-    public static ArrayList<People> queryPeopleByAge(int age, ArrayList<People> Persons) {
+    public static ArrayList<People> queryPeopleByAge() {
 
         ArrayList<People> ageFullFillingPeople = new ArrayList<>();
 
-        for(People guy: Persons){
-            int years = guy.getAge();
-            if( years <= age) {
+        for (People guy : PeopleRepository.getRepository()) {
+            if ( 18 <= guy.getAge()) {
                 ageFullFillingPeople.add(guy);
             }
         }
+
         return ageFullFillingPeople;
     }
 
-    public static void setSons(People dad, People son, People son2){
-
-        ArrayList<People> sonsList = new ArrayList<>();
-
-        sonsList.add(son);
-        sonsList.add(son2);
-        dad.setSon(sonsList);
-    }
-
-    public static void setFamilyTree() {
-        People people = PeopleRegisterService.getPeople();
-
-        People h1 = PeopleRegisterService.getPeople(1);
-        People h2 = PeopleRegisterService.getPeople(2);
-
-        People h1h1 = PeopleRegisterService.getPeople(3);
-        People h1h2 = PeopleRegisterService.getPeople(4);
-
-        People h2h1 = PeopleRegisterService.getPeople(5);
-        People h2h2 = PeopleRegisterService.getPeople(6);
-
-        PeopleRegisterService.setSons(people, h1, h2);
-
-        PeopleRegisterService.setSons(h1, h1h1, h1h2);
-
-        PeopleRegisterService.setSons(h2, h2h1, h2h2);
-    }
 }
+
+
